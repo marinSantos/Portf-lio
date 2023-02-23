4e4fb156dd7e77438a7064c84a602c11ab21b9ee
Lucas Marin <96667789+marinSantos@users.noreply.github.com>
2023-02-22 23:34:24 -0300

    Add files via upload

---
+++ ScrollView.js
@@ -0,0 +1,9 @@
+//scrollView
+ScrollReveal({ reset: true, distance: '15px' })
+
+ScrollReveal().reveal('.home', { delay: 200, duration: 1500 });
+ScrollReveal().reveal('.case', { delay: 200, origin: 'left', duration: 1500 });
+ScrollReveal().reveal('.caseR', { delay: 200, origin: 'right', duration: 1500 });
+ScrollReveal().reveal('.detalhes', { delay: 200, duration: 1500 });
+ScrollReveal().reveal('.tecs', { delay: 200, duration: 1500 });
+ScrollReveal().reveal('.slider', { delay: 200, duration: 1500 });
