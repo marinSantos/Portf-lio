44684d4aeac61332488941928f1b56ac14a1cf62
Lucas Marin <96667789+marinSantos@users.noreply.github.com>
2023-08-02 00:06:30 -0300

    Update ScrollView.js

--- ScrollView.js
+++ ScrollView.js
@@ -2,7 +2,6 @@
 ScrollReveal({ reset: true, distance: '15px' })
 
 ScrollReveal().reveal('.home', { delay: 200, duration: 1500 });
-ScrollReveal().reveal('.case', { delay: 200, origin: 'left', duration: 1500 });
-ScrollReveal().reveal('.caseR', { delay: 200, origin: 'right', duration: 1500 });
+ScrollReveal().reveal('.case', {delay: 200, duration: 1500 });
 ScrollReveal().reveal('.detalhes', { delay: 200, duration: 1500 });
 ScrollReveal().reveal('.tecs', { delay: 200, duration: 1500 });
